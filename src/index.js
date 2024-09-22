3e1d62c5ba16859deacc03814e01b1d4f53306be
de-salman <salmanfarisi.bca@gmail.com>
2024-09-22 06:45:47 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import * as THREE from "three";
-import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.161.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.161.0/examples/jsm/controls/OrbitControls.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
+
 const fov = 75;
 const aspect = w / h;
 const near = 0.1;
 const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
+
 const scene = new THREE.Scene();
 
 const controls = new OrbitControls(camera, renderer.domElement);
